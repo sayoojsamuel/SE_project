8421b1a9c3b6092eaa730fa22b425d8b7b085353
Maddela Vishnu Priya <37250846+vishnu2112@users.noreply.github.com>
2020-05-16 22:13:27 +0530

    added fields in Blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+	list_display = ('title','date_posted','author')
+	search_fields = ('title','author', )
+	
+     
+
+
+admin.site.register(Post, PostAdmin)
+
 
